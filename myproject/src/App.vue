5492074a44968a10f3a4118447804d135508c491
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-14 21:46:19 +0530

    life-cycle hooks

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,41 +2,44 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends v-bind:friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
+    <!-- <AllFriends v-bind:friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" /> -->
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllFriends from "./AllFriends";
-import OnlineFriends from "./OnlineFriends";
+// import AllFriends from "./AllFriends";
+// import OnlineFriends from "./OnlineFriends";
+import Blogs from './Blogs'
 
 export default {
   name: "app",
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends
+    Blogs
+    // OnlineFriends,
+    // AllFriends
   },
   data() {
     return {
       title: "My first Vue app, wooo :)",
-      friends: [
-        { name: "Mario", online: true },
-        { name: "Luigi", online: false },
-        { name: "Toad", online: true },
-        { name: "Bowser", online: false }
-      ]
+      // friends: [
+      //   { name: "Mario", online: true },
+      //   { name: "Luigi", online: false },
+      //   { name: "Toad", online: true },
+      //   { name: "Bowser", online: false }
+      // ]
     };
   },
   methods: {
-    deleteFriend(payload) {
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
+    // deleteFriend(payload) {
+    //   console.log(payload)
+    //   this.friends = this.friends.filter(friend => {
+    //     return friend.name !== payload.name
+    //   })
+    // }
   }
 };
 </script>
