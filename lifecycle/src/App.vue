cbf7059811bfe83ecc222f74401ca7cdae7079d7
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-14 23:34:31 +0530

    vue js lifecyle hooks

--- lifecycle/src/App.vue
+++ lifecycle/src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <Blogs />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Blogs from './components/Blogs'
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Blogs
+  },
+  data() {
+    return {
+      title: "My first Vue app, wooo :)",
+    };
+  },
+  methods: {
+    
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
+  color: #444;
+  font-weight: normal;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
