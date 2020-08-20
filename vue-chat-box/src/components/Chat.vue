dfdb7aefdb4f3a154da57f4ffed48dced48aac13
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-08-20 21:39:45 +0530

    real time events(events listener)

--- vue-chat-box/src/components/Chat.vue
+++ vue-chat-box/src/components/Chat.vue
@@ -4,37 +4,64 @@
     <div class="card">
       <div class="card-content">
         <ul class="messages">
-          <li>
-            <span class="teal-text">Name</span>
-            <span class="grey-text text-darken-3">message</span>
-            <span class="grey-text time">time</span>
+          <li v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{ message.name }}</span>
+            <span class="grey-text text-darken-3">{{ message.content }}</span>
+            <span class="grey-text time">{{ message.timestamp }}</span>
           </li>
         </ul>
       </div>
       <div class="card-content">
-          <input type="text">
+        <NewMessage :name="name" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import NewMessage from "@/components/NewMessage";
+import db from "@/firebase/init";
+
 export default {
   name: "Chat",
+  components: {
+    NewMessage,
+  },
+  data() {
+    return {
+      messages: [],
+    };
+  },
   props: ["name"],
+  created() {
+    let ref = db.collection("messages").orderBy("timestamp");
+    ref.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type == "added") {
+          let doc = change.doc;
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            content: doc.data().content,
+            timestamp: doc.data().timestamp,
+          });
+        }
+      });
+    });
+  },
 };
 </script>
 
 <style>
-.chat h2{
-    font-size: 2.6em;
-    margin-bottom: 40px;
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
 }
 .chat span {
-    font-size: 1.4em;
+  font-size: 1.4em;
 }
 .chat .time {
-    display: block;
-    font-size: 1.2em;
+  display: block;
+  font-size: 1.2em;
 }
 </style>
