e9da940f6091ae233605c2514c068423daf26d73
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-08-26 10:20:04 +0530

    add some style

--- geo-loc-app/src/components/profile/ViewProfile.vue
+++ geo-loc-app/src/components/profile/ViewProfile.vue
@@ -4,7 +4,7 @@
       <h2 class="deep-purple-text center">{{profile.alias}}'s Wall</h2>
       <ul class="comments collection">
         <li v-for="(comment, index) in comments" :key="index">
-          <div class="deep-purple-text">{{comment.from}}</div>
+          <div class="from deep-purple-text">{{comment.from}}:</div>
           <div class="grey-text text-darken-2">{{comment.content}}</div>
         </li>
       </ul>
@@ -41,7 +41,7 @@ export default {
         db.collection("comments")
           .add({
             to: this.$route.params.id,
-            from: this.user.id,
+            from: this.user.alias,
             content: this.newComment,
             time: Date.now(),
           })
@@ -93,4 +93,19 @@ export default {
 </script>
 
 <style>
+.view-profile .card {
+    padding: 20px;
+    margin-top: 60px;
+}
+.view-profile h2 {
+    font-size: 2em;
+    margin-top: 0;
+}
+.view-profile li {
+    padding: 10px;
+    border-bottom: 1px solid #eee;
+}
+.view-profile .from {
+    font-size: 1.2em;
+}
 </style>
